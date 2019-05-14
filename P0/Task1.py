5f6ac9802fb91db9db57abcdbdfce0ad9fc59a28
Hawkmoon <yfu13@ucsc.edu>
2019-05-13 17:12:57 -0700

    finished task1.py

--- P0/Task1.py
+++ P0/Task1.py
@@ -18,3 +18,18 @@ How many different telephone numbers are there in the records?
 Print a message:
 "There are <count> different telephone numbers in the records."
 """
+
+list1 = []
+list2 = []
+for i  in range(len(texts)):
+    list1.append(texts[i][0])
+    list1.append(texts[i][1])
+
+for j in range(len(calls)):
+    list2.append(calls[j][0])
+    list2.append(calls[j][1])
+
+set1 = set(list1)
+set2 = set(list2)
+count = len(set1)+ len(set2)
+print("There are", count, "different telephone numbers in the records.")
